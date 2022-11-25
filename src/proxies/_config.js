5df505625b4f22ce08b01d0755fd5a455a4985c8
tendenciasddddddddddddd <10004095632w@gmail.com>
2022-11-25 17:13:07 -0500

    first commitlpo

--- src/proxies/_config.js
+++ src/proxies/_config.js
@@ -45,7 +45,7 @@ Axios.interceptors.response.use(
     }
   );
 
-let url = 'https://uemah.com/api'; //http://localhost:3000/ https://uemah.com/api    https://uecam.net/
+let url = 'http://localhost:4000/api'; //http://localhost:3000/ https://uemah.com/api    https://uecam.net/
 // https://uehuaca.com/  https://uelibertad.online/ https://wesweb.online
 
 export default{
--- src/store/index.js
+++ src/store/index.js
@@ -10,7 +10,7 @@ const state = {
     first_text: 'Pages',
     last_text: 'Inicio',
     imgs: 'https://res.cloudinary.com/dbtlyhhcm/image/upload/v1669214496/avatar_zdsxb4.webp',
-    fechaReporter:'El Ángel, '
+    fechaReporter:'La Libertad, '
 }
 export default new Vuex.Store({
     state,
